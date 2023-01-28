ace34528115670c81dc5396e6936ce510612f28c
Britain Seright <51188389+bseright@users.noreply.github.com>
2023-01-28 12:37:35 -0600

    add right hover animation

--- script.js
+++ script.js
@@ -2,31 +2,39 @@ let faces = document.querySelectorAll(".face");
 let hexagon = document.querySelector(".hexagon");
 let container = document.querySelector(".container");
 
-let right = document.querySelector("#right");
-let left = document.querySelector("#left");
-let topFace = document.querySelector("#top");
+let right = document.querySelector(".right");
+let left = document.querySelector(".left");
+let topFace = document.querySelector(".top");
 
 let pulseRight = document.querySelector(".pulseRight");
 let pulseLeft = document.querySelector(".pulseLeft");
 let pulseTop = document.querySelector(".pulseTop");
 
-faces.forEach(face => {
-    face.addEventListener('mouseover', function() {
-        
-        right.classList.remove("pulseRight");
-        left.classList.remove("pulseLeft");
-        topFace.classList.remove("pulseTop");
+right.addEventListener('mouseover', function() {
+    
+    right.classList.remove("right");
+    right.classList.remove("pulseRight");
+    left.classList.remove("pulseLeft");
+    topFace.classList.remove("pulseTop");
 
-        hexagon.remove();
+    right.classList.add("hoverRight");
+    left.classList.add("stopPulseLeft");
+    topFace.classList.add("stopPulseTop");
 
-    })
-
-    face.addEventListener('mouseout', function() {
-        
-        right.classList.add("pulseRight");
-        left.classList.add("pulseLeft");
-        topFace.classList.add("pulseTop");
-
-        container.prepend(hexagon);
-    })
+    hexagon.remove();
 })
+
+right.addEventListener('mouseout', function() {
+    
+    right.classList.add("right");
+    right.classList.add("pulseRight");
+    left.classList.add("pulseLeft");
+    topFace.classList.add("pulseTop");
+
+    right.classList.remove("hoverRight");
+    left.classList.remove("stopPulseLeft");
+    topFace.classList.remove("stopPulseTop");
+
+    container.prepend(hexagon);
+})
+
